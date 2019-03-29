71831e39383836a8a5c734285983aeeb10e64b94
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-03-29 15:15:06 +0100

    niniLotto

---
+++ src/RandomNumber.java
@@ -0,0 +1,14 @@
+import javax.sound.midi.Soundbank;
+import java.util.Random;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int number = 0;
+        System.out.println("Drawn numbers: ");
+        for (int i = 1; i <= 6; i++) {
+            System.out.println(random.nextInt(49) + 1);
+        }
+
+    }
+}
